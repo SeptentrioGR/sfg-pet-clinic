6d419c5fc3f164a65f29fb456d5f5b3143030aa9
Petros Kitsaras <petros.kitsaras@gmail.com>
2020-02-13 16:59:07 +0100

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/thegamerurso/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package thegamerurso.springframework.sfgpetclinic.services;
+
+import org.springframework.stereotype.Service;
+import thegamerurso.springframework.sfgpetclinic.model.Speciality;
+@Service
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
