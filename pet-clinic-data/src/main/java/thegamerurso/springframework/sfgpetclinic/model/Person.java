3d72da6a3f72cb39c06b7bfeb88cb0b33e6edd9e
Petros Kitsaras <petros.kitsaras@gmail.com>
2020-03-23 19:45:28 +0100

    upgraded to JUnit 5. Closes #4

--- pet-clinic-data/src/main/java/thegamerurso/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/thegamerurso/springframework/sfgpetclinic/model/Person.java
@@ -1,12 +1,17 @@
 package thegamerurso.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-@Setter
+
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
     @Column(name = "first_name")
